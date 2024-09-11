a2b233c1d19dcb436c9a35c73c67895b7c9ddbae
Devodriq Roberts <devodriqr@gmail.com>
2024-09-11 15:09:25 -0400

    Add addtional routes and organize

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from db import items, stores
 app = Flask(__name__)
 
 
+# Stores
 @app.get("/store")
 def get_stores():
     return {"Stores": list(stores.values())}
@@ -41,6 +42,52 @@ def create_store():
     return store, 201
 
 
+@app.delete("/store/<string:store_id>") 
+def delete_store(store_id):
+    # Delete store if store exists
+    try:
+        store = stores[store_id]
+        del stores[store_id]
+        return {"message": "Store deleted."}, 200
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+
+    # Check if valid keys in payload.
+    for key in store_data.keys():
+        if key not in ["name"]:
+            abort(400, "Bad request. Be sure item only includes 'name' in the JSON payload.")
+
+    try:
+        # Get store if exists
+        store = stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+    else:
+        # Update store
+        store |= store_data
+        return store, 200
+
+
+
+
+# Items
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>") 
+def get_item(item_id):
+    # Get store; Check if store exists
+    try:
+        return items[item_id], 200
+    except KeyError:
+        abort(404, message="Item not found.")
 
 
 @app.post("/item")
@@ -70,15 +117,32 @@ def create_item():
     return item, 201
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/item/<string:item_id>") 
-def get_item(item_id):
-    # Get store; Check if store exists
+@app.delete("/item/<string:item_id>") 
+def delete_item(item_id):
+    # Delete item if item exists
     try:
-        return items[item_id], 200
+        item = items[item_id]
+        del items[item_id]
+        return {"message": "Item deleted."}, 200
     except KeyError:
         abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+
+    # Check if valid keys in payload.
+    for key in item_data.keys():
+        if key not in ["name", "price"]:
+            abort(400, "Bad request. Be sure item only includes 'name', or 'price' in the JSON payload.")
+
+    try:
+        # Get item if exists
+        item = items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+    else:
+        # Update item
+        item |= item_data
+        return item, 200
